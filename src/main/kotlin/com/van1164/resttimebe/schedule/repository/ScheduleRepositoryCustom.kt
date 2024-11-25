473ab26db8779d364a4225c1ca080c7d5bc1d18a
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-11-26 00:15:36 +0900

    chore: 리포지토리 parameter 변수명 수정

--- src/main/kotlin/com/van1164/resttimebe/schedule/repository/ScheduleRepositoryCustom.kt
+++ src/main/kotlin/com/van1164/resttimebe/schedule/repository/ScheduleRepositoryCustom.kt
@@ -4,5 +4,5 @@ import com.van1164.resttimebe.domain.Schedule
 import java.time.LocalDateTime
 
 interface ScheduleRepositoryCustom {
-    fun findSchedules(userId: String, start: LocalDateTime, end: LocalDateTime): List<Schedule>
+    fun findSchedules(userId: String, rangeStart: LocalDateTime, rangeEnd: LocalDateTime): List<Schedule>
 }
